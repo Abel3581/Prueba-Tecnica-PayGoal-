71086e1472707457ce8694d09eb0c0e5f1c070fc
Abel3581 <marzoa3581@gmail.com>
2023-11-29 19:02:08 -0300

    first commit

---
+++ src/main/java/com/test/products/entity/Product.java
@@ -0,0 +1,28 @@
+package com.test.products.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int count;
+
+    
+}
